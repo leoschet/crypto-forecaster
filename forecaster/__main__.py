53d2e238e50b4ff97962f4e9a1e6953241f9a36f
leoschet <ljsa@cin.ufpe.br>
2018-10-25 19:10:57 -0300

    Add gcForest into main

--- forecaster/__main__.py
+++ forecaster/__main__.py
@@ -13,8 +13,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 
-
 from . import classifiers, retriever, timeseries
+from .gcForest import config
+from .gcForest import GCForest
 
 # TODO: Create config json
 #       - Use data header only
@@ -22,24 +23,28 @@ cryptocurrencies = ['bitcoin']
 estimations = ['count']#, 'paper'
 data_headers = {
     'bitcoin': {
-        'price': ['positive_topic', 'total_topic', 'positive_reply', 'very_positive_topic'],
-        # 'price': ['positive_topic', 'total_topic', 'positive_reply', 'total_reply'],
+        # 'price': ['positive_topic', 'total_topic', 'positive_reply', 'very_positive_topic'],
+        'price': ['positive_topic', 'total_topic', 'positive_reply', 'total_reply'],
         # 'price': ['positive_topic', 'total_topic', 'positive_reply', 'very_positive_topic', 'total_reply'],
         # 'price': ['today_price'],
         'transactions': ['total_topic', 'very_positive_topic', 'very_positive_reply'],
-        # 'transactions': ['total_topic', 'very_positive_topic', 'very_positive_reply'],
     }
 }
 label_headers = ['price', 'transactions']
 
+## Classifiers
 aode = classifiers.AODE()
 svclassifier = SVC(C=0.5, kernel='rbf', degree=8, gamma=0.01, probability=True)
 mlp = MLPClassifier(learning_rate='adaptive', learning_rate_init=0.001)
 rfc = RandomForestClassifier(
-    n_estimators=200, criterion='gini', max_depth=20, random_state=0,
+    n_estimators=800, criterion='gini', max_depth=30, random_state=0,
     n_jobs=-1, min_samples_leaf=1
 )
 
+# GcForest
+gc_config = config.load_config()
+
+## Regressors
 # Lower gamma helps higher lags
 svregressor = SVR(C=100, kernel='rbf', degree=8, gamma=0.001)
 
@@ -107,11 +112,11 @@ for cryptocurrency in cryptocurrencies:
 
             results[cryptocurrency][label][estimation] = {}
 
-            for lag in range(1, 25):
+            for lag in range(1, 51):
                 print('\n\n\t\tPredicting with lag =', str(lag))
                 
                 # Get rolling windows of data
-                features_windows = timeseries.window_stack(data, window=lag)
+                features_windows = timeseries.window_stack(data, window=lag, spatial=False)
 
                 # Cut off unused labels, a.k.a.: before lag labels
                 lagged_labels = labels[(lag - 1):]
@@ -123,12 +128,55 @@ for cryptocurrency in cryptocurrencies:
                 train_data, test_data = features_windows[:697], features_windows[697:]
                 train_labels, test_labels = lagged_labels[:697], lagged_labels[697:]
 
+                ## GC FOREST
+                # Modify data for gcforest
+                # train_data = train_data[:, np.newaxis, :, :]
+                # test_data = test_data[:, np.newaxis, :, :]
+
+                # # Modify labels for gcforest
+                # lagged_labels = [int(float(l)) for l in lagged_labels]
+                # test_labels = [int(float(l)) for l in test_labels]
+
+                # lagged_labels = [0 if l == -1 else l for l in lagged_labels]
+                # test_labels = [0 if l == -1 else l for l in test_labels]
+
+                # # Set finegraining args for gcforest
+                # series_amount = len(data_headers[cryptocurrency][label])
+                # if lag > 31:
+                #     small_win = int(lag/16)
+                #     medium_win = int(lag/8)
+                #     large_win = int(lag/4)
+                #     stride = 2
+                # elif lag > 15:
+                #     small_win = int(lag/8)
+                #     medium_win = int(lag/4)
+                #     large_win = int(lag/2)
+                #     stride = 2
+                # elif lag > 7:
+                #     small_win = int(lag/6)
+                #     medium_win = int(lag/3)
+                #     large_win = int(lag/2)
+                #     stride = 1
+                # else:
+                #     small_win = 2
+                #     medium_win = 3
+                #     large_win = 4
+                #     stride = 1
+
+                # gc_config = config.set_finegraining_args(
+                #     gc_config, series_amount, small_win, medium_win, large_win, stride
+                # )
+
+                # # Create gcforest classifier
+                # gc = GCForest(gc_config)
+
                 # fit the classifier
                 # aode.fit(train_data, train_labels, online=False)
                 # svclassifier.fit(train_data, train_labels)
                 # mlp.fit(train_data, train_labels)
                 rfc.fit(train_data, train_labels)
                 # svregressor.fit(train_data, train_labels)
+                # gc.fit_transform(train_data, train_labels)
 
                 # predict
                 pred_labels = []
@@ -138,6 +186,7 @@ for cryptocurrency in cryptocurrencies:
                 # pred_labels = mlp.predict(test_data)
                 pred_labels = rfc.predict(test_data)
                 # pred_labels = svregressor.predict(test_data)
+                # pred_labels = gc.predict(test_data)
 
                 # Regressor execution:
                 # plot(list(test_labels), list(pred_labels))
